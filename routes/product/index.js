fa2add893e523436aa3f9f5718791658ff760e97
Kevin Ponder <kevin_ponder@homedepot.com>
2017-05-03 13:46:59 -0400

    Updated Show Routes

--- routes/product/index.js
+++ routes/product/index.js
@@ -2,11 +2,30 @@ const Product = require('../../models/product');
 const Transaction = require('../../models/transaction');
 const router = require('express').Router();
 
-
+// INDEX
 router.get('/', function(req, res, next) {
-  Product.fetchAll().then((product) => {
-    res.json(product);
-    });
+  Product.fetchAll().then( (products) => {
+    res.status(200).json(products);
+  }).catch( (err) => {
+    console.log(err);
+    res.status(500).json(err);
   });
+});
 
-//module.exports = router;
+// SHOW
+router.get('/:id', function(req, res, next) {
+  Product.where({id: req.params.id}).fetch ().then( (product) => {
+    if (!product) {
+
+      res.status(404).json({ message: 'Product not found'});
+    }
+    else {
+      res.status(200).json(product);
+    }
+  }).catch( (err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
