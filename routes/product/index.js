bfcb366585b761fbc3196c3e7be3844e181263e6
Kevin Ponder <kevin_ponder@homedepot.com>
2017-05-03 14:13:48 -0400

    Updated all routes

--- routes/product/index.js
+++ routes/product/index.js
@@ -28,4 +28,45 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
+
+// CREATE
+router.post('/', (req, res, next) => {
+  Movie.forge(req.body).save().then( (movie) => {
+    res.json(movie);
+  }).catch( (err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// UPDATE
+router.put('/:id', (req, res, next) => {
+  Movie.forge({'id': req.params.id}).save(req.body).then( (movie) => {
+    res.json(movie);
+  }).catch( (err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// DESTROY
+router.delete('/:id', (req, res, next) => {
+  Movie.where({id: req.params.id}).fetch().then( (movie) => {
+    if (!movie) {
+      res.status(404).json({ message: 'Movie not found'});
+    }
+    else {
+      movie
+      .destroy()
+      .then( (movie) => {
+        res.json(movie);
+      }).catch( (err) => {
+        console.log(err);
+        res.status(500).json();
+      });
+    }
+  });
+});
+
+
 module.exports = router;
